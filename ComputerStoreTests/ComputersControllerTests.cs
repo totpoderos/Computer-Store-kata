cd3329cd3434681f4ec26aacead85b5eb6e1d481
Marcos <marcos.graup@gmail.com>
2020-10-12 19:26:38 +0200

    Add get computer and image info tests

--- ComputerStoreTests/ComputersControllerTests.cs
+++ ComputerStoreTests/ComputersControllerTests.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using ComputerStore;
 using ComputerStore.Controllers;
 using ComputerStore.Controllers.Response;
 using NUnit.Framework;
@@ -21,6 +20,52 @@ namespace ComputerStoreTests
             
             Assert.AreEqual(5, computers.Count);
             Assert.AreEqual(expectedComputerName, computers.First().Name);
+        }
+
+        [Test]
+        public void Get_computer_detailed_information_by_guid()
+        {
+            ComputersController computersController = new ComputersController();
+            string computerName = "MacBook Pro 15";
+
+            ComputerInfoDto computer = computersController.GetComputerInfo(computerName);
+            
+            Assert.AreEqual(computerName, computer.Name);
+            Assert.AreEqual("Macbook pro 15' Description", computer.Description);
+            Assert.AreEqual(1500, computer.Price);
+        }
+        
+        [Test]
+        public void Raise_an_error_when_searching_a_non_existing_computer()
+        {
+            ComputersController computersController = new ComputersController();
+            string computerName = "Unknown";
+
+            Exception exception = Assert.Throws<Exception>( () => computersController.GetComputerInfo(computerName));
+            Assert.AreEqual("Computer not found, name: Unknown", exception.Message);
+        }
+        
+        [Test]
+        public void Get_computer_image()
+        {
+            ComputersController computersController = new ComputersController();
+            string computerName = "iMac 27";
+            string expectedSecondLineImageContet = "        " + " *    * " + "        " + "        " + "        " +
+                                                   " ****** " + " ********";
+
+            ComputerInfoDto computerInfo = computersController.GetComputerInfo(computerName);
+
+            ComputerImageInfoDto imageInfo = computersController.GetComputerImage(computerInfo.Id);
+
+            Assert.AreEqual(expectedSecondLineImageContet, imageInfo.Content[1]);
+        }
+        
+        [Test]
+        public void Raise_an_error_when_searching_image_info_of_non_existing_computer()
+        {
+            ComputersController computersController = new ComputersController();
+            Exception exception = Assert.Throws<Exception>(() => computersController.GetComputerImage("unknown_guid"));
+            Assert.AreEqual("Computer not found, guid: unknown_guid", exception.Message);
         }
     }
 }
